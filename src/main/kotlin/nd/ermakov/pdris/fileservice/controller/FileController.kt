0503fb4beb399f2f04536eba83ae4447ff660abc
kit-rex <ermakov.nd@phystech.edu>
2022-04-11 19:00:30 +0300

    Controller fixed

--- src/main/kotlin/nd/ermakov/pdris/fileservice/controller/FileController.kt
+++ src/main/kotlin/nd/ermakov/pdris/fileservice/controller/FileController.kt
@@ -2,6 +2,7 @@ package nd.ermakov.pdris.fileservice.controller
 
 import nd.ermakov.pdris.fileservice.model.File
 import nd.ermakov.pdris.fileservice.service.FileService
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,13 +14,13 @@ import org.springframework.web.multipart.MultipartFile
 @RequestMapping("/file")
 class FileController(private val fileService: FileService) {
 
-    @GetMapping
+    @GetMapping(produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE])
     fun getFile(@RequestParam filename: String): ByteArray {
         return fileService.getFile(filename).data
     }
 
-    @PostMapping
-    fun putFile(@RequestParam file: MultipartFile) {
-        fileService.putFile(File(file.name, file.bytes))
+    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun putFile(@RequestParam filename: String, @RequestParam file: MultipartFile) {
+        fileService.putFile(File(filename, file.bytes))
     }
 }
